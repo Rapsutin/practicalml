54dafabf9597fa1cf9ddacce50c0db28a8ab437b
Rapsutin <juho.lamminmaki@gmail.com>
2017-02-20 20:44:54 +0200

    Debug -> False

--- practicalml/settings.py
+++ practicalml/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f#1boxv!2$67701%*4yam-odp9l)r^)8(#@@02)yjmnt7n4e2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["nginx"]
 
